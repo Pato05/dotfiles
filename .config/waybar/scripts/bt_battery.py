c68be73bc72c64f0ed20db97a8cd9380d79507a2
Pato05 <pato05mc@gmail.com>
2022-09-09 22:03:00 +0200

    waybar/bt_battery: use upower dbus interface instead of bluez

--- .config/waybar/scripts/bt_battery.py
+++ .config/waybar/scripts/bt_battery.py
@@ -1,84 +1,98 @@
 #!/usr/bin/python3
 # i hate python :)
 from time import sleep
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
 import dbus
 import json
 import sys
-# dbus-send --print-reply=literal --system --dest=org.bluez /org/bluez/hci0/dev_<MAC> org.freedesktop.DBus.Properties string:"org.bluez.Battery1" string:"Percentage"
+
+DBusGMainLoop(set_as_default=True)
 bus = dbus.SystemBus()
 
-INTERVAL = 30
+INTERVAL = 30000 # 30 seconds
+UPOWER_IFACE = 'org.freedesktop.UPower'
+UPOWER_PATH = '/org/freedesktop/UPower'
+UPOWER_DEVICE_IFACE = UPOWER_IFACE + '.Device'
+
+__devices_check: list[str] = []
+is_loop_running = False
 
 
-def get_device_list(manager):
-    # try to get the entire list of connected devices
-    # https://raspberrypi.stackexchange.com/a/114173
-    managed_objs = manager.GetManagedObjects(
-        dbus_interface='org.freedesktop.DBus.ObjectManager')
-    devices = []
-    for path in managed_objs:
-        uuids = managed_objs[path].get(
-            'org.bluez.Device1', {}).get('UUIDs', [])
-        # print(path, uuids)
-        for uuid in uuids:
-            # Service discovery UUIDs
-            # https://www.bluetooth.com/specifications/assigned-numbers/service-discovery/
-            # AudioSink - 0x110B - Advanced Audio Distribution Profile (A2DP)
-            if uuid.startswith('0000110b'):
-                connected = managed_objs[path].get(
-                    'org.bluez.Device1', {}).get('Connected', False)
-                if connected:
-                    devices.append({
-                        'name': managed_objs[path].get('org.bluez.Device1', {}).get('Name'),
-                        'address': managed_objs[path].get('org.bluez.Device1', {}).get('Address')
-                    })
-    return devices
+def start_loop():
+    global is_loop_running
+    get_battery_infos()
+    if is_loop_running:
+        return
+    is_loop_running = True
+    GLib.timeout_add(INTERVAL, get_battery_infos)
 
-
-def get_info(devices, adapterPath='/org/bluez/hci0'):
+def get_battery_infos():
+    global is_loop_running
+    if not __devices_check:
+        is_loop_running = False
+        return False # destroy timeout in event loop
+    
     device_percentages = []
     device_msgs = []
-    for device in devices:
-        try:
-            dev = bus.get_object(
-                "org.bluez", f"{adapterPath}/dev_{device['address'].replace(':', '_')}")
-            perc_ = dev.Get("org.bluez.Battery1", "Percentage",
-                            dbus_interface='org.freedesktop.DBus.Properties')
+    for device in __devices_check:
+        device_obj = bus.get_object(UPOWER_IFACE, device)
+        device_iface = dbus.Interface(device_obj, dbus.PROPERTIES_IFACE)
+    
+        device_name = device_iface.Get(UPOWER_DEVICE_IFACE, 'Model')
+        if not device_name:
+            device_name = device_iface.Get(UPOWER_DEVICE_IFACE, 'Serial')
+        
+        percentage = int(device_iface.Get(UPOWER_DEVICE_IFACE, 'Percentage'))
+        device_percentages.append(percentage)
+        device_msgs.append(f'{device_name}: {percentage}%')
+        
+    print(json.dumps({
+        'percentage': min(device_percentages),
+        'tooltip': '\n'.join(device_msgs)
+    }), flush=True)
+    return True # continue running
 
-            percentage = int(perc_)
-            device_percentages.append(percentage)
-            device_msgs.append(f'{device["name"]}: {percentage}%')
-        except Exception as e:
-            print(e)
-            pass
-    return device_msgs, device_percentages
 
 
-def wait_for_device(manager, poll_interval=15):
-    devices = []
-    while len(devices) == 0:
-        devices = get_device_list(manager)
-        if len(devices) == 0:
-            sys.stdout.write('\n')
-            sys.stdout.flush()
-        else:
-            return devices
-        sleep(poll_interval)
-    return devices
+def __device_connected(device_name, call_timeout=True):
+    global __devices_check
+    device_obj = bus.get_object(UPOWER_IFACE, device_name)
+    native_path: str = device_obj.Get(UPOWER_DEVICE_IFACE, 'NativePath', dbus_interface=dbus.PROPERTIES_IFACE)
+    if not native_path.startswith('/org/bluez'):
+        # we don't care about a non-bluetooth device
+        return
+    __devices_check.append(device_name)
+    if len(__devices_check) == 1 and call_timeout:
+        start_loop()
+
+
+
+def __device_disconnected(device_name):
+    global __devices_check
+    try:
+        __devices_check.remove(device_name)
+        if not __devices_check:
+            print('', flush=True)
+    except ValueError:
+        pass
 
 
 def main():
-    manager = bus.get_object("org.bluez", "/")
-    while True:
-        devices = wait_for_device(manager)
-        device_msgs, device_percentages = get_info(devices)
-        avg_percentage = int(sum(device_percentages) / len(device_percentages))
-        sys.stdout.write(json.dumps({
-            'tooltip': '\n'.join(device_msgs),
-            'percentage': avg_percentage
-        }) + '\n')
-        sys.stdout.flush()
-        sleep(INTERVAL)
+    upower_obj = bus.get_object(UPOWER_IFACE, UPOWER_PATH)
+    devices = upower_obj.EnumerateDevices(dbus_interface=UPOWER_IFACE)
+    for device in devices:
+        __device_connected(device, call_timeout=False)
+    if devices:
+        start_loop()
+
+    bus.add_signal_receiver(__device_connected, bus_name='org.freedesktop.UPower',
+                            signal_name='DeviceAdded')
+    bus.add_signal_receiver(__device_disconnected, bus_name='org.freedesktop.UPower',
+                        signal_name='DeviceRemoved')
+    loop = GLib.MainLoop()
+
+    loop.run()
 
 
 if __name__ == '__main__':
